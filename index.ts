7e74d16903cbdfbde0f20cd8425c050b07b285b2
AnassMoumni <159035398+AnassMoumni@users.noreply.github.com>
2024-03-01 17:09:36 +0000

    objecten aangemaakt 2

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import pokemon from './pokemon.json';
 import abilities from './abilities.json';
 
 // Lees de inhoud van Pokemon.json en Abilities.json
-const pokemonData: Pokemon[] = JSON.parse(pokemon.readFileSync('Pokemon.json', 'utf8'));
-const abilitiesData: Ability[] = JSON.parse(abilities.readFileSync('Abilities.json', 'utf8'));
+//const pokemonData: Pokemon[] = JSON.parse(pokemon.readFileSync('Pokemon.json', 'utf8'));
+//const abilitiesData: Ability[] = JSON.parse(abilities.readFileSync('Abilities.json', 'utf8'));
 
 interface Pokemon {
     id: number;
@@ -29,10 +29,9 @@ console.log('1. View all data');
 console.log('2. Filter by ID');
 console.log('3. Exit');
 
-for (let index = 0; index < pokemonData.length; index++) {
-    console.log(`- ${pokemon[i].name} (${pokemon[i].id})`);
+/*for (let index = 0; index < pokemonData.length; index++) {  console.log(`- ${pokemon[i].name} (${pokemon[i].id})`);
 
-}
+}*/
 
 
 
